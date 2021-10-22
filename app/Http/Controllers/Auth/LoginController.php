02ef4d9a875a6a97625e75ad613017bc9dc80b7e
EC2 Default User <ec2-user@ip-172-31-39-1.ap-northeast-1.compute.internal>
2021-10-22 22:24:27 +0900

    edit-google-login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -58,7 +58,7 @@ class LoginController extends Controller
             $user = $this->createUserByGoogle($gUser);
         }
         // ログイン処理
-        \Auth::login($user, true);
+        Auth::login($user, true);
         return redirect('/');
     }
     
@@ -67,7 +67,7 @@ class LoginController extends Controller
         $user = User::create([
             'name'     => $gUser->name,
             'email'    => $gUser->email,
-            'password' => \Hash::make(uniqid()),
+            'password' => Hash::make(uniqid()),
         ]);
         return $user;
     }
